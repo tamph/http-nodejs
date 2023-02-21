33681236b1b6711ea26d6aaf2d8173f5e0c91ada
Nomi Las <tamph08@gmail.com>
2023-02-21 23:47:26 +0100

    a

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var router = express.Router();
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
 // });
-router.get('/increase').get(controller.increase)
-router.get('/').get(controller.value)
+router.route('/').get(controller.value)
+router.route('/increase').get(controller.increase)
 
 module.exports = router;
