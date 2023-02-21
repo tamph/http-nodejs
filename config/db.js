70946dd8059deff351b5c9bfcd173913ff278840
Nomi Las <tamph08@gmail.com>
2023-02-21 21:07:34 +0100

    start

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'containers-us-west-94.railway.app',
+    user: 'root',
+    database:'railway',
+    password: 'IEaSsW6CLZfLwSu8nTBS',
+})
+let sql = 'SELECT * FROM clicky;';
+pool.execute(sql, function(err,res){
+    if(err) throw err;
+    console.log(res);
+}); 
+module.exports = pool.promise();
